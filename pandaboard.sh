54fda5bc1bf37b48fcd6097b05dbad23aa16f9e1
mhhm2005eg <mhhm2005eg@yahoo.com>
2014-12-01 17:30:44 +0100

    Adding more steps and reference  ..

--- pandaboard.sh
+++ pandaboard.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
+#http://openframeworks.cc/setup/pandaboard/
+sudo su -
+sudo su add-apt-repository ppa:tiomap-dev/release
 sudo apt-get update
 sudo apt-get dist-upgrade
 sudo apt-get install ubuntu-omap4-extras
+/usr/bin/alsaucm -c Panda set _verb HiFi
+#sudo /usr/sbin/flash-kernel --update-bootloader
+#lsmod
+#sudo apt-get install --reinstall pvr-omap4-dkms
+#there are some manual modifications related to the bootloader
+
+sudo apt-get install xubuntu-desktop
